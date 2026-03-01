377457a2dba31dbfa62ad4b3ed38f9ba94cb4648
jamie <jadeantonis@gmail.com>
2026-03-01 15:35:34 -0500

    another resource

--- simulstreaming/whisper/simul_whisper/whisper/audio.py
+++ simulstreaming/whisper/simul_whisper/whisper/audio.py
@@ -102,9 +102,9 @@ def mel_filters(device, n_mels: int) -> torch.Tensor:
     """
     assert n_mels in {80, 128}, f"Unsupported n_mels: {n_mels}"
 
-    filters_path = resources.files("simul_whisper.whisper").joinpath(
-        "assets/mel_filters.npz"
-    )
+    filters_path = resources.files(
+        "simulstreaming.whisper.simul_whisper.whisper"
+    ).joinpath("assets/mel_filters.npz")
     with np.load(filters_path, allow_pickle=False) as f:
         return torch.from_numpy(f[f"mel_{n_mels}"]).to(device)
 
